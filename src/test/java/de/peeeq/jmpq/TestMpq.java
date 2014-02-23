2645a7f10c686b5c1b0bcdbc523fa9f188b3868b
Peter Zeller <Peter.peq@googlemail.com>
2014-02-24 00:30:25 +0100

    tests

--- src/test/java/de/peeeq/jmpq/TestMpq.java
+++ src/test/java/de/peeeq/jmpq/TestMpq.java
@@ -13,7 +13,7 @@ import com.google.common.io.Files;
 public class TestMpq {
 
 	File map = new File("map.w3m");
-	
+	File map2 = new File("map2.w3x");
 	
 	@Test
 	public void testExtract() throws IOException {
@@ -31,5 +31,66 @@ public class TestMpq {
 	}
 	
 	// TODO more tests
+	
+	@Test
+	public void testRemoveAddExtract() throws IOException, InterruptedException {
+		File test = new File("test2.w3m");
+		Files.copy(map2, test);
 
+		File temp = new File("temp.w3u");
+		File temp2 = new File("temp2.w3u");
+		File temp3 = new File("temp3.w3u");
+		temp.delete();
+		temp2.delete();
+		temp3.delete();
+		
+		
+		Files.write("Hello world", temp2, Charsets.UTF_8);
+		
+		// extract w3u:
+		new Main(new String[] {"extract", test.getAbsolutePath(), "war3map.w3u", temp.getAbsolutePath()});
+		
+		new Main(new String[] {"delete", test.getAbsolutePath(), "war3map.w3u"});
+		new Main(new String[] {"compact", test.getAbsolutePath()});
+		
+		
+		new Main(new String[] {"add", test.getAbsolutePath(), temp2.getAbsolutePath(), "war3map.w3u"});
+		
+		
+		new Main(new String[] {"extract", test.getAbsolutePath(), "war3map.w3u", temp3.getAbsolutePath()});
+		
+		String s = Files.toString(temp3, Charsets.UTF_8);
+		
+		assertTrue(s.contains("Hello world"));
+	}
+
+	
+	@Test
+	public void testOverwrite() throws IOException, InterruptedException {
+		File test = new File("test2.w3m");
+		Files.copy(map2, test);
+
+		File temp = new File("temp.w3u");
+		File temp2 = new File("temp2.w3u");
+		File temp3 = new File("temp3.w3u");
+		temp.delete();
+		temp2.delete();
+		temp3.delete();
+		
+		
+		Files.write("Hello world", temp2, Charsets.UTF_8);
+		
+		// extract w3u:
+		new Main(new String[] {"extract", test.getAbsolutePath(), "war3map.w3u", temp.getAbsolutePath()});
+		
+		new Main(new String[] {"add", test.getAbsolutePath(), temp2.getAbsolutePath(), "war3map.w3u"});
+		
+		
+		new Main(new String[] {"extract", test.getAbsolutePath(), "war3map.w3u", temp3.getAbsolutePath()});
+		
+		String s = Files.toString(temp3, Charsets.UTF_8);
+		
+		assertTrue(s.contains("Hello world"));
+	}
+	
 }
